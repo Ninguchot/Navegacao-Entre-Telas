a5e40d72c3c9069044116192c75cd8bc9c9665b6
Anderson Almeida <110561674+Ninguchot@users.noreply.github.com>
2023-10-03 20:35:58 -0300

    Feito o binding para ir da segunda tela para a primeira tela

--- app/src/main/java/com/example/navegaoentretelas/SegundaTela.kt
+++ app/src/main/java/com/example/navegaoentretelas/SegundaTela.kt
@@ -1,11 +1,22 @@
 package com.example.navegaoentretelas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navegaoentretelas.databinding.ActivitySegundaTelaBinding
 
 class SegundaTela : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySegundaTelaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda_tela)
+        binding = ActivitySegundaTelaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.icVoltar.setOnClickListener{
+            val voltarTelaPrincipal = Intent(this,MainActivity::class.java)
+            startActivity(voltarTelaPrincipal)
+        }
     }
 }
